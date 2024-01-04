8fd922f637609adf04882d043592be5c5c8ec97a
Emery Coughtry <ecoughtry@ataleia.ca>
2024-01-04 10:12:18 -0800

    Remove tenant

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -13,7 +13,6 @@ use Spatie\Permission\Guard;
 use Spatie\Permission\PermissionRegistrar;
 use Spatie\Permission\Traits\HasPermissions;
 use Spatie\Permission\Traits\RefreshesPermissionCache;
-use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
 
 
 /**
@@ -24,7 +23,6 @@ class Role extends Model implements RoleContract
 {
     use HasPermissions;
     use RefreshesPermissionCache;
-    use BelongsToTenant;
 
     protected $guarded = [];
 
